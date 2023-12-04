b1d51f117570f9f1269018208bcad90e9981f1a7
ethanholbrook <ethanwholbrook@gmail.com>
2023-12-04 00:56:50 -0500

    Materials Project API calls for structures works.

--- gui.py
+++ gui.py
@@ -7,24 +7,36 @@ from tkinter.font import Font
 # from reportlab.pdfgen import canvas
 import csv
 from models import get_matgl_formation_energy
-from inputs import get_structure_from_file
+
+from inputs import get_structure_from_file, get_structure_from_MatProj
+from inputs import make_structure_image
+import os
+from PIL import Image, ImageTk
 
 text_visualization = None  # Define text_visualization as a global variable
-
+structure = None
+ase_atoms = None
+# entry_url = None
+# entry_api = None
 
 def predict_properties():
     global text_visualization  # Access the global variable
     if text_visualization is not None:
         # Get test structure from file
-        structure = get_structure_from_file("example_files/NaCl.cif")
-        # Predict properties
-        final_structure, final_energy, eform = get_matgl_formation_energy(structure)
-        # Update the "Visualization Area" with the results
-        text_visualization.delete(1.0, tk.END)  # Clear the previous content
-        text_visualization.insert(
-            tk.END,
-            f"The final relaxed structure is:\n{final_structure}\n\nThe final energy is {float(final_energy):.3f} eV.\n\nThe predicted formation energy for this structure is {float(eform.numpy()):.3f} eV/atom.",
-        )
+        # structure = get_structure_from_file("example_files/NaCl.cif")
+        # structure object is now create when load button is pressed
+        try:
+            # Predict properties
+            final_structure, final_energy, eform = get_matgl_formation_energy(structure)
+            # Update the "Visualization Area" with the results
+            text_visualization.delete(1.0, tk.END)  # Clear the previous content
+            text_visualization.insert(
+                tk.END,
+                f"The final relaxed structure is:\n{final_structure}\n\nThe final energy is {float(final_energy):.3f} eV.\n\nThe predicted formation energy for this structure is {float(eform.numpy()):.3f} eV/atom.",
+            )
+        except Exception as e:
+            text_visualization.delete(1.0, tk.END) 
+            text_visualization.insert(tk.END, f"Error predicting properties: {e}")
 
 
 def export_to_pdf():
@@ -74,16 +86,48 @@ def change_font():
     )  # Open a file dialog to select a font file
     text_visualization.config(font=font)
 
+def load_structure(entry_url, entry_api):
+    global structure
+    global ase_atoms
+    
+    try:
+        url = entry_url.get()
+        api_key = entry_api.get()
+        mp_id = url.split('/')[-1]    
+        structure, ase_atoms = get_structure_from_MatProj(api_key, mp_id)
+        text_visualization.delete(1.0, tk.END) 
+        text_visualization.insert(tk.END, "Loaded Structure from Materials Project.")
+        return structure, ase_atoms
+    except Exception as e:
+        text_visualization.delete(1.0, tk.END) 
+        text_visualization.insert(tk.END, 
+                                  f"API key and/or URL entered incorrectly: {e}")
+        return None
+    
+def visualize_structure():
+    global image_placeholder
+    make_structure_image(ase_atoms, img="mp_load.png")
+    image = Image.open('mp_load.png')
+    desired_size = (250, 250)
+    image = image.resize(desired_size, Image.ANTIALIAS)
+    
+    tk_structure_image = ImageTk.PhotoImage(image)
+    image_placeholder.config(image=tk_structure_image)
+    image_placeholder.image = tk_structure_image
+    # structure_image = tk.Label(frame, image=tk_structure_image)
+    # structure_image.image = tk_structure_image
+    # structure_image.pack(side=tk.TOP, padx=10)  # Adjust positioning
 
 def test_GUI():
     root = tk.Tk()
     root.title("GNN Model Selector")
 
     global text_visualization  # Access the global variable
-
+    global image_placeholder
+    
     # Create a frame to hold the entire content
     frame = tk.Frame(root)
-    frame.pack(fill="both", expand=True)
+    frame.grid(padx=10, pady=10)
 
     # Create a style for custom theming
     style = ttk.Style()
@@ -93,71 +137,99 @@ def test_GUI():
     style.map("TButton", background=[("active", "#005A8C")])
 
     # Input for Crystal Structure Data URL
-    label_url = tk.Label(frame, text="Enter Crystal Structure Data URL:")
-    label_url.pack(pady=10)
+    label_url = tk.Label(frame, text=("Enter Crystal Structure Data URL:\n"
+                                      'https://materialsproject.org/materials/mp-22851\n'
+                                      'or\n'
+                                      'mp-22851'))
+    label_url.grid(row=0, column=0, columnspan=2, pady=5)
     entry_url = tk.Entry(frame, width=50)
-    entry_url.pack(pady=10)
+    entry_url.grid(row=1, column=0, columnspan=2, pady=5)
 
+    # Input box for API KEY 
+    label_api = tk.Label(frame, text="Enter Materials Project API key:")
+    label_api.grid(row=2, column=0, columnspan=2, pady=5)
+    entry_api = tk.Entry(frame, width=50)
+    entry_api.grid(row=3, column=0, columnspan=2, pady=5)
+    
+    # Load Structure Button
+    load_structure_properties = tk.Button(
+        frame, text="Load Structure",     
+        command=lambda: load_structure(entry_url, entry_api)
+    )
+    load_structure_properties.grid(row=4, column=0, pady=5)
+    
+    # Image Placeholder
+    image_placeholder = tk.Label(frame)
+    image_placeholder.config(width=50, height=25, bg='gray')  
+    image_placeholder.grid(row=1, column=3, columnspan=3,rowspan=10, pady=10)
+    
+    #Visualize Structure Button
+    visualize_structure_properties = tk.Button(
+        frame, text="Visualize Structure",
+        command=visualize_structure
+    )
+    visualize_structure_properties.grid(row=4, column=1, pady=5)
+    
     # Dropdown for Materials Project Data
     label_materials = tk.Label(frame, text="Materials Project Data:")
-    label_materials.pack(pady=10)
+    label_materials.grid(row=5, column=0, pady=5)
     materials_var = tk.StringVar()
     materials_dropdown = ttk.Combobox(
         frame, textvariable=materials_var, values=["Data1", "Data2", "Data3"]
     )
-    materials_dropdown.pack(pady=10)
+    materials_dropdown.grid(row=6, column=0, columnspan=2, pady=5)
 
     # Dropdown for GNN Models
     label_gnn = tk.Label(frame, text="Select Graph Neural Network Model:")
-    label_gnn.pack(pady=10)
+    label_gnn.grid(row=7, column=0, pady=5)
     gnn_var = tk.StringVar()
     gnn_dropdown = ttk.Combobox(
         frame,
         textvariable=gnn_var,
         values=["GNN Model 1", "GNN Model 2", "GNN Model 3"],
     )
-    gnn_dropdown.pack(pady=10)
+    gnn_dropdown.grid(row=8, column=0, columnspan=2, pady=5)
 
     # Visualization Area with Scrollbar
     label_visualization = tk.Label(frame, text="Visualization of the Process:")
-    label_visualization.pack(pady=10)
+    label_visualization.grid(row=9, column=0, pady=5)
     text_visualization = tk.Text(frame, height=10, width=50)
-    text_visualization.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    text_visualization.grid(row=10, column=0, columnspan=2)
     scrollbar = tk.Scrollbar(frame, command=text_visualization.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    scrollbar.grid(row=10, column=2, sticky='ns')
     text_visualization.config(yscrollcommand=scrollbar.set)
 
     # Customization Buttons
     btn_change_text_color = tk.Button(
         frame, text="Change Text Color", command=change_text_color
     )
-    btn_change_text_color.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_change_text_color.grid(row=11, column=0, pady=5)
 
     btn_change_background_color = tk.Button(
         frame, text="Change Background Color", command=change_background_color
     )
-    btn_change_background_color.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_change_background_color.grid(row=11, column=1, pady=5)
 
     btn_change_font = tk.Button(frame, text="Change Font", command=change_font)
-    btn_change_font.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_change_font.grid(row=12, column=0, columnspan=2, pady=5)
 
     # Export Buttons
     btn_export_pdf = tk.Button(frame, text="Export to PDF", command=export_to_pdf)
-    btn_export_pdf.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_export_pdf.grid(row=13, column=0, pady=5)
 
     btn_export_csv = tk.Button(frame, text="Export to CSV", command=export_to_csv)
-    btn_export_csv.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_export_csv.grid(row=13, column=1, pady=5)
 
     # Documentation Button
     btn_documentation = tk.Button(
         frame, text="Documentation and Help Center", command=open_documentation
     )
-    btn_documentation.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_documentation.grid(row=14, column=0, columnspan=2, pady=5)
 
     # Prediction Button
     btn_predict_properties = tk.Button(
         frame, text="Predict Properties", command=predict_properties
     )
-    btn_predict_properties.pack(side=tk.TOP, padx=10)  # Adjust positioning
+    btn_predict_properties.grid(row=15, column=0, columnspan=2, pady=5)
 
     root.mainloop()
--- inputs.py
+++ inputs.py
@@ -1,9 +1,28 @@
-from pymatgen.core.structure import Structure
+# from pymatgen.core.structure import Structure
+from mp_api.client import MPRester
+import matplotlib.pyplot as plt
+from ase.io import read
+from ase.visualize import view
+from ase.visualize.plot import plot_atoms
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen.io.cif import CifParser
 
+def get_structure_from_MatProj(API_key, mp_id):
+    mpr = MPRester(API_key)
+    structure = mpr.get_structure_by_material_id(mp_id)
+    ase_atoms = AseAtomsAdaptor().get_atoms(structure)
 
-def get_structure_from_file(filename):
+    return structure, ase_atoms
+
+def make_structure_image(ase_atoms, img="ase_temp.png"):
+    fig, ax = plt.subplots()
+    plot_atoms(ase_atoms, ax, radii=0.3, rotation=("0x,0y,0z"))
+    plt.show()
+    fig.savefig(img)
+
+def get_structure_from_file(file):
     """
-    Load a Structure object from a file.
+    Load a Structure object from a file. Also output ase_atoms object. 
 
     Args:
         filename (str): The name of the file to load the Structure from.
@@ -11,4 +30,22 @@ def get_structure_from_file(filename):
     Returns:
         pymatgen.core.structure.Structure: The loaded Structure object.
     """
-    return Structure.from_file(filename)
+    parser = CifParser(file)
+    structure = parser.parse_structures()[0]
+    ase_atoms = AseAtomsAdaptor().get_atoms(structure)
+
+    return structure, ase_atoms
+
+def load_API_key():
+    '''From text box or file.'''
+    with open('../api_key.txt','r') as api_file:
+        API_KEY = api_file.readlines()[0].strip()
+    
+    return API_KEY
+    
+def Ethan():
+    API_key = API_KEY
+    mp_id = "mp-22897"
+
+    s1, ase1 = get_structure_from_MatProj(API_key, mp_id)
+
